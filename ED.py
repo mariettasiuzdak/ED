48c1ca938ce1753c1cc675797f40f9ce359f8440
mariettasiuzdak <marietta.siuzdak@gmail.com>
2018-01-21 00:56:02 +0100

    Add files via upload

--- ED.py
+++ ED.py
@@ -15,18 +15,7 @@ def closest(data, v):
 #WCZYTUJĘ DANE
 sensor_locations = genfromtxt('Airly_data/sensor_locations.csv', delimiter=',')
 furnaces = genfromtxt('Piece/CSV/WS_PIECE_17_BUD_cvs.csv', delimiter=',', usecols=range(9))
-airly_january = genfromtxt('Airly_data/january-2017.csv', delimiter=',')
-airly_february = genfromtxt('Airly_data/february-2017.csv', delimiter=',')
-airly_march = genfromtxt('Airly_data/march-2017.csv', delimiter=',')
-airly_april = genfromtxt('Airly_data/april-2017.csv', delimiter=',')
-airly_may = genfromtxt('Airly_data/may-2017.csv', delimiter=',')
-airly_june = genfromtxt('Airly_data/june-2017.csv', delimiter=',')
-airly_july = genfromtxt('Airly_data/july-2017.csv', delimiter=',')
-airly_august = genfromtxt('Airly_data/august-2017.csv', delimiter=',')
-airly_september = genfromtxt('Airly_data/september-2017.csv', delimiter=',')
-airly_october = genfromtxt('Airly_data/october-2017.csv', delimiter=',')
-airly_november = genfromtxt('Airly_data/november-2017.csv', delimiter=',')
-airly_december = genfromtxt('Airly_data/december-2017.csv', delimiter=',')
+airly_all = genfromtxt('Airly_data/all-2017.csv', delimiter=';', dtype='unicode')
 
 #DEKLARUJĘ TABLICE DO PRZECHOWYWANIA WSPÓŁRZĘDNYCH GEOGRAFICZNYCH
 sensor_coordinates=[]
@@ -44,37 +33,33 @@ for x in range(1,row_count_sensors):
 #NASTĘPNIE SZUKAM DLA DANEGO MIEJSCA Z PIECEM NAJBLIŻSZEGO SENSORA I PRINTUJĘ GO
 row_count = sum(1 for row in furnaces)
 for x in range(1,row_count):
-    if furnaces[x, 6] == 0:
-        number_of_furnaces = furnaces[x, 6]
-        lon = furnaces[x,0]
-        lat = furnaces[x,1]
-        furnace_coordinates = {'lat':lat, 'lon': lon}
-        closest_sensor=closest(sensor_coordinates, furnace_coordinates)
-       # print(closest_sensor['lat'])
+    #if furnaces[x, 6] != 0:
+    number_of_furnaces = int(furnaces[x,6])
+    lon = furnaces[x,0]
+    lat = furnaces[x,1]
+    furnace_coordinates = {'lat':lat, 'lon': lon}
+    closest_sensor=closest(sensor_coordinates, furnace_coordinates)
 
 #Szukam ID najbliższego sensora
-        for y in range(1,row_count_sensors):
-            if sensor_locations[y,1] == closest_sensor['lat'] and sensor_locations[y,2] == closest_sensor['lon']:
-                sensor_id = sensor_locations[y,0]
+    for y in range(1,row_count_sensors):
+        if sensor_locations[y,1] == closest_sensor['lat'] and sensor_locations[y,2] == closest_sensor['lon']:
+            sensor_id = sensor_locations[y,0]
 
-        column_count_january = sum(1 for column in airly_january.T)
-        # to nie działa jeszcze :D chodzi tu o to, żeby znaleźć kolumnę, w której są dane pyłowe dla danego sensora
-        # jest problem z enkodowaniem, naprawię później
-        for z in range(1,column_count_january):
-            print(airly_january[1,8])
-            exit(0)
-            if airly_january[1,z] == sensor_id + "_pm25":
-                print("tu")
+    column_count_airly = sum(1 for column in airly_all.T)
+    row_count_airly = sum(1 for column in airly_all)
+    pm25 = []
+    #  znajdujemy kolumnę, w której są dane pyłowe dla danego sensora, tworzymy tablicę z danymi z sensora dla danego pieca
 
-
-
-
+    for z in range(0,column_count_airly):
+        if airly_all[0,z] == str(int(sensor_id)) + "_pm25":
+            for w in range(1,row_count_airly):
+                pm25.append(airly_all[w,z])
+# teraz każdemu miejscu przypisana jest ilość pieców i tablica z zapyleniem
+    print(pm25)
+    print(number_of_furnaces)
 
 
 '''
 TODO
-Skoro mamy już najbliższy sensor przypisany do pieca, musimy pobrać ID danego sensora na podstawie współrzędnych, a potem na podstawie ID znaleźć
-wartośći PM2.5 przypisane do tego sensora z całego roku.
-
-Następnie będziemy liczyć współczynnik określający jakąś proporcję między zapyleniem a ilością pieców w regionie
+Liczenie współczynnika określającego jakąś proporcję między zapyleniem a ilością pieców w regionie
 '''
