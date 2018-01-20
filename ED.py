669584c64f129c3b694d0f8543f164ad6b804b6f
mariettasiuzdak <marietta.siuzdak@gmail.com>
2018-01-20 13:58:23 +0100

    Add files via upload

--- ED.py
+++ ED.py
@@ -15,6 +15,18 @@ def closest(data, v):
 #WCZYTUJĘ DANE
 sensor_locations = genfromtxt('Airly_data/sensor_locations.csv', delimiter=',')
 furnaces = genfromtxt('Piece/CSV/WS_PIECE_17_BUD_cvs.csv', delimiter=',', usecols=range(9))
+airly_january = genfromtxt('Airly_data/january-2017.csv', delimiter=',')
+airly_february = genfromtxt('Airly_data/february-2017.csv', delimiter=',')
+airly_march = genfromtxt('Airly_data/march-2017.csv', delimiter=',')
+airly_april = genfromtxt('Airly_data/april-2017.csv', delimiter=',')
+airly_may = genfromtxt('Airly_data/may-2017.csv', delimiter=',')
+airly_june = genfromtxt('Airly_data/june-2017.csv', delimiter=',')
+airly_july = genfromtxt('Airly_data/july-2017.csv', delimiter=',')
+airly_august = genfromtxt('Airly_data/august-2017.csv', delimiter=',')
+airly_september = genfromtxt('Airly_data/september-2017.csv', delimiter=',')
+airly_october = genfromtxt('Airly_data/october-2017.csv', delimiter=',')
+airly_november = genfromtxt('Airly_data/november-2017.csv', delimiter=',')
+airly_december = genfromtxt('Airly_data/december-2017.csv', delimiter=',')
 
 #DEKLARUJĘ TABLICE DO PRZECHOWYWANIA WSPÓŁRZĘDNYCH GEOGRAFICZNYCH
 sensor_coordinates=[]
@@ -38,8 +50,27 @@ for x in range(1,row_count):
         lat = furnaces[x,1]
         furnace_coordinates = {'lat':lat, 'lon': lon}
         closest_sensor=closest(sensor_coordinates, furnace_coordinates)
-        print(closest_sensor)
-        #exit(0)
+       # print(closest_sensor['lat'])
+
+#Szukam ID najbliższego sensora
+        for y in range(1,row_count_sensors):
+            if sensor_locations[y,1] == closest_sensor['lat'] and sensor_locations[y,2] == closest_sensor['lon']:
+                sensor_id = sensor_locations[y,0]
+
+        column_count_january = sum(1 for column in airly_january.T)
+        # to nie działa jeszcze :D chodzi tu o to, żeby znaleźć kolumnę, w której są dane pyłowe dla danego sensora
+        # jest problem z enkodowaniem, naprawię później
+        for z in range(1,column_count_january):
+            print(airly_january[1,8])
+            exit(0)
+            if airly_january[1,z] == sensor_id + "_pm25":
+                print("tu")
+
+
+
+
+
+
 '''
 TODO
 Skoro mamy już najbliższy sensor przypisany do pieca, musimy pobrać ID danego sensora na podstawie współrzędnych, a potem na podstawie ID znaleźć
